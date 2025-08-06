778f6959eb05a22d06bc515dac49737e00708311
Hazim Bakar <hazimbaa@gmail.com>
2025-08-06 22:38:26 +0800

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+// to get built-in security features with various http headers
+app.use(helmet());
+
+// to log HTTP requests in the console
+app.use(morgan("dev"));
+
+// to allow cross-origin requests
+// this is useful when the frontend and backend are hosted on different domains or ports
+// it enables the backend to accept requests from the frontend
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  console.log(res.getHeaders());
+  res.send("Hello, World!");
+});
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Welcome to the API!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
